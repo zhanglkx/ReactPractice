5879cbf223d291d09b3abefe516d3d5e6fdd8ce7
zhanglike <zhanglkx@foxmail.com>
2024-01-23 23:36:37 +0800

    shuuk

--- src/Components/Login/IconView/index.tsx
+++ src/Components/Login/IconView/index.tsx
@@ -3,7 +3,7 @@ import Icon from "@/Components/Icon";
 const index: React.FC = () => {
   return (
     <>
-      <div className="flex flex-wrap justify-center w-full h-20 pt-36">
+      <div className="flex flex-wrap justify-center w-full h-20 mt-40">
         <div className="items-center justify-center h-16 aliw-full">
           <Icon className="w-16 h-16 " type="icon-dixiaofei" />
         </div>
--- src/Components/Login/InputView/index.tsx
+++ src/Components/Login/InputView/index.tsx
@@ -1,18 +1,30 @@
 import { Input } from "antd";
+import React, { useState } from "react";
 
-const index: React.FC = () => {
+const Index: React.FC = () => {
+  const [visibility, setVisibility] = useState<boolean>(true);
+//   setVisibility(!visibility);
+
+
+
   return (
     <>
-      <div className="w-full h-16 pt-5 bg-pink">
-        <div>
-          <Input allowClear placeholder="Basic usage" />
+      <div className="flex flex-col flex-wrap justify-around w-full mt-10 h-28 bg-pink">
+        <div className="w-full h-8">
+          <Input activeBg='#000000' allowClear placeholder="请输入手机号" />
         </div>
-        <div>
-          <Input.Password allowClear placeholder="Basic usage" />
+        <div className="w-full h-8">
+          <Input.Password
+            visibilityToggle={visibility}
+            allowClear
+            placeholder="去输入密码"
+          />
         </div>
       </div>
     </>
   );
 };
 
-export default index;
+
+
+export default Index;
