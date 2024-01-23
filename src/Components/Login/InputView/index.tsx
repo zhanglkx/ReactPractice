30bf169f8b22f7fa1e6cf126aeedba56381dd5b2
zhanglike <zhanglkx@foxmail.com>
2024-01-23 23:41:17 +0800

    sh 输入框点击事件回调

--- src/Components/Login/InputView/index.tsx
+++ src/Components/Login/InputView/index.tsx
@@ -2,22 +2,28 @@ import { Input } from "antd";
 import React, { useState } from "react";
 
 const Index: React.FC = () => {
-  const [visibility, setVisibility] = useState<boolean>(true);
+  const [visibility, setVisibility] = useState<boolean>(false);
 //   setVisibility(!visibility);
 
+const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  console.log(e.target.value);
+  alert(e.target.value);
+  setVisibility(true);
+};
 
 
   return (
     <>
       <div className="flex flex-col flex-wrap justify-around w-full mt-10 h-28 bg-pink">
         <div className="w-full h-8">
-          <Input activeBg='#000000' allowClear placeholder="请输入手机号" />
+          <Input allowClear placeholder="请输入手机号" onChange={inputChange}/>
         </div>
         <div className="w-full h-8">
           <Input.Password
             visibilityToggle={visibility}
             allowClear
             placeholder="去输入密码"
+            onChange={inputChange}
           />
         </div>
       </div>
