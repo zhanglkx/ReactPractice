09ab4a4d3d7d6d7e397d8d400d34ef1a2977008e
zhanglike <zhanglkx@foxmail.com>
2024-01-24 22:26:13 +0800

    🌟feat: (InputView) 👉  verify view Input

--- src/Components/Login/InputView/index.tsx
+++ src/Components/Login/InputView/index.tsx
@@ -1,13 +1,13 @@
 // /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Input } from "antd";
 import React, { useState } from "react";
-import "./index.css";
+import "./style.module.css";
 import { InputType } from "@/Components/Login/Interface/interface";
-const { Search } = Input;
+// const { Search } = Input;
 
 type PasswordViewProps = {
   visibility: boolean;
-  inputChange: (e:any)=>void;
+  inputChange: (e: any) => void;
 };
 
 const Index: React.FC = () => {
@@ -22,17 +22,17 @@ const Index: React.FC = () => {
   };
   let Verify;
   if (inputType === InputType.Password) {
-    Verify = <PasswordView visibility={visibility} inputChange={inputChange}/>;
+    Verify = <PasswordView visibility={visibility} inputChange={inputChange} />;
   } else {
     Verify = <VerificationView />;
   }
 
   return (
     <>
-      <div className="flex flex-col flex-wrap justify-around w-5/6 h-20 mt-14 bg-pink">
+      <div className="flex flex-col flex-wrap justify-around w-5/6 h-28 mt-14">
         <div className="w-full h-7">
           <Input
-            className="rounded-full"
+            className="h-10 rounded-full"
             allowClear
             placeholder="请输入手机号"
             onChange={inputChange}
@@ -44,13 +44,18 @@ const Index: React.FC = () => {
   );
 };
 
-const PasswordView = ({visibility,inputChange}: PasswordViewProps) => {
+/**
+ *  密码界面
+ * @param param0 {visibility:boolean;inputChange:(e:any)=>void;}
+ * @returns 密码界面
+ */
+const PasswordView = ({ visibility, inputChange }: PasswordViewProps) => {
   return (
     <>
       <div>
         <div className="w-full h-7 bg-custom">
           <Input.Password
-            className="rounded-full"
+            className="h-10 rounded-full"
             visibilityToggle={visibility}
             allowClear
             placeholder="去输入密码"
@@ -63,17 +68,25 @@ const PasswordView = ({visibility,inputChange}: PasswordViewProps) => {
     </>
   );
 };
-
-const VerificationView: React.FC = () => {
+/**
+ * 验证码界面
+ * @returns 验证码界面
+ */
+const VerificationView = () => {
   return (
     <>
-      <div>
-        <Search
-          placeholder="input search text"
-          enterButton="Search"
+      <div className="relative overflow-hidden rounded-full bg-custom">
+        <Input
+          style={{ borderRadius: "30px" }}
+          className="w-full h-10 rounded-full"
+          placeholder="请输入验证码"
+          allowClear
           size="large"
-          loading
+          // onSearch={onSearch}
         />
+        <div className="absolute top-0 right-0 flex items-center justify-center h-full w-28 bg-pink">
+          发送验证码
+        </div>
       </div>
     </>
   );
