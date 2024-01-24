a7b4fbb441634d7a11449a53a4efd78955f69e40
zhanglike <zhanglkx@foxmail.com>
2024-01-24 21:11:48 +0800

    🌟feat: (dependencies) 👉 config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime"
     ],
-  },
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
+        '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+    }
 }
