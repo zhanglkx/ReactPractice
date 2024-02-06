7513a3a6585499bc54dbd4a8f52e657f95231ffd
zhanglike <zhanglkx@foxmail.com>
2024-02-06 22:54:37 +0800

    🌟 feat:【hook】👉 useReducer+ immer

--- src/hooks/useage/Reducer.tsx
+++ src/hooks/useage/Reducer.tsx
@@ -1,36 +1,52 @@
 import { Reducer, useReducer } from "react";
+import { produce } from 'immer'
 
 interface Data {
-    result: number;
+    a: {
+        c: {
+            e: number,
+            f: number
+        },
+        d: number
+    },
+    b: number
 }
 
 interface Action {
-    type: 'add' | 'minus',
+    type: 'add',
     num: number
 }
+
 function reducer(state: Data, action: Action) {
 
     switch (action.type) {
         case 'add':
-            return {
-                result: state.result + action.num
-            }
-        case 'minus':
-            return {
-                result: state.result - action.num
-            }
+            return produce(state, (state) => {
+                state.a.c.e += action.num
+            })
+
     }
     return state;
 }
 
 function App() {
-    const [res, dispatch] = useReducer<Reducer<Data, Action>>(reducer, { result: 0 });
+    const [res, dispatch] = useReducer<Reducer<Data, Action>, string>(reducer, 'zero', (param) => {
+        return {
+            a: {
+                c: {
+                    e: 0,
+                    f: 0
+                },
+                d: 0
+            },
+            b: 0
+        }
+    });
 
     return (
         <div>
             <div onClick={() => dispatch({ type: 'add', num: 2 })}>加</div>
-            <div onClick={() => dispatch({ type: 'minus', num: 1 })}>减</div>
-            <div>{res.result}</div>
+            <div>{JSON.stringify(res)}</div>
         </div>
     );
 }
