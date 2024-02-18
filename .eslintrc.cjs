bfe0701d90280bcc4f01e6c8978333984382b026
zhanglkx <zhanglkx@foxmail.com>
2024-02-18 16:28:09 +0800

    memo + useMemo + useCallback
    memo + useMemo + useCallback

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     ],
     "rules": {
         '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
-        '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+        '@typescript-eslint/no-unused-vars': 'off', // 禁止定义未使用的变量
+
     }
 }
--- src/hooks/useage/memo + useMemo + useCallback.tsx
+++ src/hooks/useage/memo + useMemo + useCallback.tsx
@@ -1,7 +1,18 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
+// memo 是防止 props 没变时的重新渲染，useMemo 和 useCallback 是防止 props 的不必要变化。
+
+/**
+ *  memo 包裹组件，当组件参数不变化的时候，不会重新渲染
+ *  useMemo 作用是防止 props 没变时的重新渲染。可以使用useMemo 包裹props，有点类似 vue 的 computed
+ * useCallback 是作用就是当 deps 数组不变的时候，始终返回同一个 function，当 deps 变的时候，才把 function 改为新传入的。
+ * @returns {JSX.Element}
+ */
 
 function Aaa() {
     const [, setNum] = useState(1);
+
+    const [count, setCount] = useState(2);
+
 
     useEffect(() => {
         setInterval(() => {
@@ -9,8 +20,18 @@ function Aaa() {
         }, 2000)
     }, []);
 
+
+
+    /**
+     * 因为每次 function 都是新创建的，也就是每次 props 都会变，这样 memo 就没用了。
+     */
+    const bbbCallback = useCallback(function () {
+        // xxx
+    }, []);
+
+
     return <div>
-        <MemoBbb count={2}></MemoBbb>
+        <MemoBbb count={count} callback={bbbCallback}></MemoBbb>
     </div>
 }
 
@@ -20,6 +41,7 @@ interface BbbProps {
 
 function Bbb(props: BbbProps) {
     console.log('bbb render');
+
     return <h2>{props.count}</h2>
 }
 
