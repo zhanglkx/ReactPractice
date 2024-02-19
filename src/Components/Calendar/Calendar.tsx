c6730d684d6c7c69f3e25721fd46fd618bbf50aa
zhanglkx <zhanglkx@foxmail.com>
2024-02-19 15:06:35 +0800

    Update Calendar.tsx

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,20 +1,11 @@
+
 import { Button } from "antd"
 import { useState } from "react";
 
 function Calendar() {
-    return (
-        <div className="h-80 w-80 border-2 border-solid border-color-lime-400 flex flex-col ">
-            <CalendarHeader />
-            <CalendarWeek />
-            <CalendarContent />
-        </div>
-    )
-}
-
-
-function CalendarHeader() {
 
     const [date, setDate] = useState(new Date());
+
 
     const handlePrevMonth = () => {
         setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
@@ -24,16 +15,39 @@ function CalendarHeader() {
         setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
     };
 
+    return (
+        <div className="h-80 w-80 border-2 border-solid border-color-lime-400 flex flex-col ">
+            <CalendarHeader handleNextMonth={handleNextMonth} handlePrevMonth={handlePrevMonth} date={date} />
+            <CalendarWeek />
+            <CalendarContent />
+        </div>
+    )
+}
+
+
+function CalendarHeader({ handlePrevMonth, handleNextMonth, date }) {
+
+    const monthNames = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月',];
+
+
+    const handlePreClick = () => {
+
+        handlePrevMonth(date);
+    };
+
+    const handleNextClick = () => {
+        handleNextMonth(date);
+    };
 
     return (
         <div className="flex justify-between flex-wrap h-7">
             <div >
-                <Button type="primary" onClick={handlePrevMonth}> {`<`} </Button>
+                <Button type="primary" onClick={handlePreClick}> {`<`} </Button>
             </div>
 
-            <div>{date.toLocaleDateString()}</div>
+            <div>{date.getFullYear()} {monthNames[date.getMonth()]} </div>
 
-            <div><Button type="primary" onClick={handleNextMonth}>{`>`}</Button></div>
+            <div><Button type="primary" onClick={handleNextClick}>{`>`}</Button></div>
         </div>
     )
 }
@@ -41,7 +55,7 @@ function CalendarHeader() {
 
 function CalendarWeek() {
 
-    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
     return (
         <div className="flex justify-between flex-wrap h-7">
@@ -59,6 +73,24 @@ function CalendarWeek() {
 }
 
 function CalendarContent() {
+
+
+    const daysOfMonth = (year: number, month: number) => {
+        return new Date(year, month + 1, 0).getDate()
+    }
+
+    const weekOfDay = (year: number, month: number) => {
+        return new Date(year, month, 1).getDay();
+    };
+
+    const renderDay = (year: number, month: number) => {
+
+        const days = [];
+
+        const dayCount = daysOfMonth(year, month);
+
+    }
+
     const arr = new Array(42).fill(0)
     return (
         <div className="flex justify-between flex-wrap w-full bg-color-amber-100 flex-1" >
