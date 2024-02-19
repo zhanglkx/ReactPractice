4b54657c7ede3bfd13ada0c5579568fc0d07bd1b
zhanglkx <zhanglkx@foxmail.com>
2024-02-19 13:12:39 +0800

    日历

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,14 +1,36 @@
 import { Button } from "antd"
+import { useState, useEffect } from "react"
 
 function Calendar() {
     return (
-        <div className="h-80 w-80 bg-color-slate-300  border-2 border-solid border-color-lime-400">
+        <div className="h-80 w-80 border-2 border-solid border-color-lime-400">
             <CalendarHeader />
             <CalendarWeek />
             <CalendarContent />
+            <Page />
         </div>
     )
 }
+
+
+function Page() {
+    const [value, setValue] = useState(1)
+    console.log(b)
+    useEffect(() => {
+        console.log(value)
+        setValue(2)
+    }, [])
+    var b = 8
+    console.log(b - value)
+    return (
+        <div className="h-80 w-80 border-2 border-solid border-color-lime-400">
+
+        </div>
+    )
+}
+
+
+
 
 
 
@@ -19,7 +41,7 @@ function CalendarHeader() {
                 <Button type="primary"> {`<`} </Button>
             </div>
 
-            <div>{`1111`}</div>
+            <div>{new Date().toLocaleDateString()}</div>
 
             <div><Button type="primary">{`>`}</Button></div>
         </div>
@@ -28,8 +50,21 @@ function CalendarHeader() {
 
 
 function CalendarWeek() {
+
+    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
     return (
-        <div>Calendar</div>
+        <div className="flex justify-between flex-wrap">
+            {
+                days.map((day, index) => {
+                    return (
+                        <div key={index} className="flex justify-center">
+                            {day}
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
