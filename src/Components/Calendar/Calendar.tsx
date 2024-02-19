b6c19adf464dc3ad8af67f2c8724302d118bb3b9
zhanglkx <zhanglkx@foxmail.com>
2024-02-19 10:14:29 +0800

    日历组件
    日历组件

---
+++ src/Components/Calendar/Calendar.tsx
@@ -0,0 +1,33 @@
+function Calendar() {
+    return (
+        <div className="h-80 w-80 bg-color-red-200">
+            <CalendarHeader />
+            <CalendarWeek />
+            <CalendarContent />
+        </div>
+    )
+}
+
+
+
+function CalendarHeader() {
+    return (
+        <div>Calendar</div>
+    )
+}
+
+
+function CalendarWeek() {
+    return (
+        <div>Calendar</div>
+    )
+}
+
+function CalendarContent() {
+    return (
+        <div>Calendar</div>
+    )
+}
+
+
+export default Calendar
