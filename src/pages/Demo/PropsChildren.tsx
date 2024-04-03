44cb472a779479982928261361c2d4c3bdd8b696
zhanglkx <zhanglkx@foxmail.com>
2024-04-03 14:57:37 +0800

    通过props，上级组件向下级组件传参children

--- src/pages/Demo/PropsChildren.tsx
+++ src/pages/Demo/PropsChildren.tsx
@@ -8,6 +8,13 @@ function Container(props: any) {
     // 这种情况，在 Container 中， props.children 属性访问到是函数，并不是 React element 对象，针对这种情况，像下面这种情况下 children 是不能直接渲染的，直接渲染会报错。
     //   return  props.children
 
+
+    /**
+     * 这种方式作用是：
+     *
+     * 1 根据需要控制 Children 渲染与否。
+     * 2 可以将需要传给 Children 的 props 直接通过函数参数的方式传递给执行函数 children 。
+     */
     // 方法 1
     // return  props.children(ContainerProps)
     // 方法二
@@ -19,10 +26,12 @@ function Container(props: any) {
     )
 }
 
-function Child() {
+function Child(props: any) {
     return (
         <div>
             子类
+            { props.name}
+            {props.mes}
         </div>
     )
 }
