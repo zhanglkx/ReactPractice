cb886390130de0e5dce742a0d8da91ef70a7850e
zhanglkx <zhanglkx@foxmail.com>
2024-04-08 15:58:38 +0800

    🌟 feat:【】👉 tailwind 设置

--- src/Components/Calendar/index.tsx
+++ src/Components/Calendar/index.tsx
@@ -1,8 +1,9 @@
 function Index() {
     return (
-        <div className='w-full h-100vh bg-main'>
-            <h1>日历组件</h1>
+        <div className='h-full bg-main outline outline-offset-2 outline-1 m-2 rounded-sm'>
+            <h1 className='text-center align-middle'>日历组件</h1>
             <MouthCalendar/>
+            <h1>日历组件</h1>
         </div>
     );
 }
@@ -11,10 +12,10 @@ function Index() {
 function MouthCalendar() {
     const weekList = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
     return <>
-        <div>
+        <div className='border-0 border-b border-blue border-solid'>
             <div className='flex justify-around'>
                 {weekList.map((week, _) => (
-                    <div key={week}>{week}</div>
+                    <div className='p-2 text-fontColor text-center flex-1 bg-color-amber-200 m-3 rounded' key={week}>{week}</div>
                 ))}
             </div>
         </div>
